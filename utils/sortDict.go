55fed0d1cc83de1c9e07f210589b51aff7dfe53f
MoonNarga <MoonNarga@outlook.com>
2022-09-25 11:58:52 +0800

    add priority for length of words

--- utils/sortDict.go
+++ utils/sortDict.go
@@ -12,8 +12,10 @@ func SortDict(dicts [][]string) {
 				v1, _ := strconv.Atoi(dicts[i][2])
 				v2, _ := strconv.Atoi(dicts[j][2])
 				return v1 > v2
+			} else if len(dicts[i][0]) == len(dicts[j][0]) {
+				return dicts[i][0] < dicts[j][0]
 			}
-			return dicts[i][0] < dicts[j][0]
+			return len(dicts[i][0]) < len(dicts[j][0])
 		}
 		return dicts[i][1] < dicts[j][1]
 	})
